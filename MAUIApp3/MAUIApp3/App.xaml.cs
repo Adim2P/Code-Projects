ebcf2774981fc71a6de954e354d31ecdb5e4a30e
Adim2P <miguellicauco@gmail.com>
2023-10-12 02:47:53 +0800

    Updated Repo, for MAUIApp3

---
+++ MAUIApp3/MAUIApp3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIApp3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MAUIApp3/MAUIApp3/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MAUIApp3
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsPhoneNumberValid(string input)
+        {
+            string validChars = "0123456789+";
+
+            return input.All(c => validChars.Contains(c));
+        }
+
+        private void OnDial(object sender, EventArgs e)
+        {
+            try
+            {
+                string phoneNumber = PhoneNum.Text;
+
+                if (IsPhoneNumberValid(phoneNumber))
+                {
+                    PhoneDialer.Open(phoneNumber);
+                }
+                else
+                {
+                    DisplayAlert("Invalid Input", "Please enter a valid phone number", "OK");
+                }
+            }
+            catch (Exception error)
+            {
+                DisplayAlert("Error", error.Message, "OK");
+            }
+        }
+    }
+}
