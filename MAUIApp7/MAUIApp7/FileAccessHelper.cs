b1f28971b6f684acb2cec10a1db8ac4314c95316
Adim2P <miguellicauco@gmail.com>
2024-01-16 23:30:41 +0800

    New MAUI App Added

---
+++ MAUIApp7/MAUIApp7/FileAccessHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MAUIApp7
+{
+    public static class FileAccessHelper
+    {
+        public static string GetLocalFilePath(string filename)
+        {
+            return Path.Combine(FileSystem.AppDataDirectory, filename);
+        }
+    }
+}
---
+++ MAUIApp7/MAUIApp7/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Maui.Controls;
+using MAUIApp7.Models;
+using System.IO;
+
+//Co Author - Janrey Buitre
+
+namespace MAUIApp7
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnNewButtonClicked(object sender, EventArgs e)
+        {
+            statusMessage.Text = "";
+            App.StudentRepo.AddNewStudent(newStudent.Text);
+            statusMessage.Text = App.StudentRepo.StatusMessage;
+        }
+
+        void OnGetButtonClicked(object sender, EventArgs e) 
+        {
+            statusMessage.Text = "";
+            List<Student> section = App.StudentRepo.GetSection();
+            sectionList.ItemsSource = section;
+        }
+
+        // alisin mo lahat yan -Buitre
+        // Di sinabi yan sa ano? Di tinanggal ka agad - Buitre
+        // Mag salita ka na pre - Buitre
+
+    }
+}
